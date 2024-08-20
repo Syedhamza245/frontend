cb1d331542ba53af19f284ff824f0e8c5bc7dba5
Syedhamza245 <155845626+Syedhamza245@users.noreply.github.com>
2024-08-19 18:53:09 -0700

    Update Filter.jsx

--- src/pages/Filter.jsx
+++ src/pages/Filter.jsx
@@ -14,7 +14,6 @@ const Filter = () => {
   const [tuition, setTuition] = useState("");
   const [prog, setProg] = useState("");
 
-
   useEffect(() => {
     fetchData();
   }, [country, duration, scholarship, ielts, tuition, prog]);
